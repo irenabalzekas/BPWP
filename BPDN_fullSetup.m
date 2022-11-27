36fe805fa7b3917efd249020a9c87412673e0b42
grace-y <50380991+grace-y@users.noreply.github.com>
2022-11-26 21:30:51 -0500

    Formatting updates

--- BPDN_fullSetup.m
+++ BPDN_fullSetup.m
@@ -1,9 +1,9 @@
-% Wrapper for other BPDN functions, setup, calculating bases, etc. 
+% DESCRIPTION: Wrapper for other BPDN functions, setup, calculating bases, etc. 
 
-function [phi, N, t_full, f_k, desiredperioddays, dct_basis, scalefactor, poly_basis] = BPDN_fullSetup(t,dt,maxperiod,minperiod,sampletype,maxdegree)
+function [phi, N, t_full, f_k, desiredperioddays, dct_basis, scalefactor, poly_basis] = BPDN_fullSetup(t, dt, maxperiod, minperiod, sampletype, maxdegree)
 
     [phi, N, t_full] = BPDN_setupData(t, dt);
-    [f_k, desiredperioddays] = frequency_sampling(N,maxperiod, minperiod, dt, sampletype);
+    [f_k, desiredperioddays] = frequency_sampling(N, maxperiod, minperiod, dt, sampletype);
     [dct_basis, scalefactor] = DCT2_basis(N, f_k); % making the basis takes a long time. want to pre-define it before running BPDN for loops and loops 
     [poly_basis] = polynomial_basis(N, maxdegree);
 
