2a5030275f7845cc9b0ac9d2ceccfcaf977ccfdd
grace-y <50380991+grace-y@users.noreply.github.com>
2022-11-26 21:33:22 -0500

    Formatting updates

--- BPDN_setupData.m
+++ BPDN_setupData.m
@@ -1,13 +1,10 @@
-% function to define phi, N, etc.
-
+% DESCRIPTION: Function to format samples and timestamps for method; i.e. define phi, N, etc.
 
 function [phi, N, t_full] = BPDN_setupData(t, dt)
-
 
     N = floor((max(t) - min(t))/dt)+1;
     T_0 = min(t); % scale sample time array (based on distance in time from start-time)
     phi = floor((t-T_0)/dt) + 1; % indices for when each time data were sampled
     t_full = min(phi):N; % initialize full time array (temporal context, as if regularly sampled)
-
 
 end 
