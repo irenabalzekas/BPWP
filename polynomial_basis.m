4b8735abf65a2e69f13e262f5aa1b0e5fe175e9d
grace-y <50380991+grace-y@users.noreply.github.com>
2022-12-01 14:53:07 -0600

    Formatting updates

--- polynomial_basis.m
+++ polynomial_basis.m
@@ -1,11 +1,8 @@
-% IB 7/26/22
+% DESCRIPTION: Function to create polynomial basis based on N and maximum desired polynomial degree
 
 function [poly_basis] = polynomial_basis(N, maxdegree)
 
-
     poly_basis = fliplr(vander([1:N]));
-    poly_basis = poly_basis(:,1:(maxdegree+1)); % degrees are represented in vandermonde as ith column having i-1 degree 
+    poly_basis = poly_basis(:, 1:(maxdegree+1)); % degrees are represented in vandermonde as ith column having i-1 degree 
 
 end 
-
-
